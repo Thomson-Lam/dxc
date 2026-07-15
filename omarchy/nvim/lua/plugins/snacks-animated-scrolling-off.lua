331cbea317fefded15468f6ba4a9215b5999b5c3
Thomson Lam <thomson.lam327@gmail.com>
2026-07-14 23:01:07 -0500

    feat (zsh, nvim): nvim-pi-blink, zsh pir command alias

--- omarchy/nvim/lua/plugins/snacks-animated-scrolling-off.lua
+++ omarchy/nvim/lua/plugins/snacks-animated-scrolling-off.lua
@@ -1,8 +1,18 @@
+local blink_review = vim.env.BLINK_MODE == "slow" or vim.env.BLINK_MODE == "blitz"
+
 return {
 	"folke/snacks.nvim",
 	opts = {
 		scroll = {
 			enabled = false, -- Disable scrolling animations
+		},
+		notifier = {
+			top_down = not blink_review,
+			margin = {
+				top = 0,
+				right = 1,
+				bottom = 0,
+			},
 		},
 	},
 }
